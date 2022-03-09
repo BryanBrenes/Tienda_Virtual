9e453bf75ea582afbc71034a34cb1a3dde7deb84
bryan <bryan@LAPTOP-FAAJDEVP>
2022-03-09 16:56:32 -0600

    Clase Semana 5 y 6

--- src/main/java/com/Tienda/controller/PersonasController.java
+++ src/main/java/com/Tienda/controller/PersonasController.java
@@ -11,6 +11,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class PersonasController {
@@ -19,9 +22,30 @@ public class PersonasController {
     
     @GetMapping("/personas")
     public String index(Model model){
-    List<Persona> listaPersonas=personaService.getAllPerson();
-    model.addAttribute("titulo","Personas");
-    model.addAttribute("personas", listaPersonas);
-    return "personas";
+        List<Persona> listaPersonas=personaService.getAllPerson();
+        model.addAttribute("titulo","Personas");
+        model.addAttribute("personas", listaPersonas);
+        return "personas";
     }
+    
+    @GetMapping("/personasN")
+    public String crearPersona(Model model){
+        model.addAttribute("persona", new Persona());
+        return "crear";
+    }
+    
+    @PostMapping("/save")
+    public String guardarPersona(@ModelAttribute Persona persona){
+        personaService.savePerson(persona);
+        return "redirect:/personas";
+    }
+    
+    @GetMapping ("/delete/{id}")
+    public String eliminarPersona(@PathVariable("id") Long idPersona){
+        personaService.delete(idPersona);
+        return "/redirect:/personas";
+    }
+    
+    
+            
 }
